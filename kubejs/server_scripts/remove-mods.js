1f1726c7a8ce8f69b8f942d25fca484703a6b63c
3x1t-5tyl3 <61988002+3x1t-5tyl3@users.noreply.github.com>
2021-08-20 19:17:36 +0200

    kubejs primary commit of files.

---
+++ kubejs/server_scripts/remove-mods.js
@@ -0,0 +1,17 @@
+// priority: 0
+
+
+onEvent('recipes', event => {
+    event.remove({mod: 'ae2extras'})
+    event.remove({mod: 'angelring'})
+    event.remove({mod: 'appliedenergistics2'})
+    event.remove({mod: 'botanypots'})
+    event.remove({mod: 'botanytrees'})
+    event.remove({mod: 'charginggadgets'})
+    event.remove({mod: 'ceramics'})
+    event.remove({mod: 'chisel'})
+    event.remove({mod: 'mekanism'})
+    event.remove({mod: 'colossalchests'})
+    event.remove({mod: 'constructionwand'})
+    event.remove({mod: 'create'})
+})
