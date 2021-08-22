f45b711b7c0c617c1bccb7790a6253765e2a49ba
3x1t-5tyl3 <61988002+3x1t-5tyl3@users.noreply.github.com>
2021-08-22 15:18:01 +0200

    Updated configs and added mods

--- kubejs/server_scripts/remove-mods.js
+++ kubejs/server_scripts/remove-mods.js
@@ -2,16 +2,5 @@
 
 
 onEvent('recipes', event => {
-    event.remove({mod: 'ae2extras'})
-    event.remove({mod: 'angelring'})
-    event.remove({mod: 'appliedenergistics2'})
-    event.remove({mod: 'botanypots'})
-    event.remove({mod: 'botanytrees'})
-    event.remove({mod: 'charginggadgets'})
-    event.remove({mod: 'ceramics'})
-    event.remove({mod: 'chisel'})
-    event.remove({mod: 'mekanism'})
-    event.remove({mod: 'colossalchests'})
-    event.remove({mod: 'constructionwand'})
-    event.remove({mod: 'create'})
+
 })
