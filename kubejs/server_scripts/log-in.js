eb43b3b3764253297440ecd24e6229aa7151573d
3x1t-5tyl3 <61988002+3x1t-5tyl3@users.noreply.github.com>
2022-04-29 22:55:45 +0200

    Update log-in.js
    am dumb, updated it to not open every time.

--- kubejs/server_scripts/log-in.js
+++ kubejs/server_scripts/log-in.js
@@ -1,9 +1,5 @@
 onEvent("player.logged_in", event => {
-    if (event.player.stages.has("first_login")) {
-        event.server.scheduleInTicks(50, callback => {
-            event.player.sendData("first_login", { message: "Welcome, select your gender!" })
-        })
-    } else {
+    if (!event.player.stages.has("first_login")) {
         event.player.stages.add("first_login");
         event.player.sendData("first_login", { message: "Welcome, select your gender!" })
     }
